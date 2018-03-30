e23c481f61186828e5a9bd7801188a917b01a309
Ian Pollack <ianjpollack@gmail.com>
2018-03-30 00:36:52 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,17 @@
 require "pry"
+
+def begins_with_r(array)
+  q = 0
+  i = 0
+  until i == array.length
+    if array[i].start.with?("r")
+      q += 1
+    end
+    i += 1
+  end
+  if q == array.length
+    return true
+  else
+    return false
+  end
+end
